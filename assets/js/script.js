bcc9b10954ca08ffd9f25ae2d522d1456bfbdaed
amclean1 <lex213075@aim.com>
2025-02-15 21:05:31 -0500

    fixing alert logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
+const dupeDatabase = {
+    "airpods": "https://www.amazon.com/dp/B08XY4H2QK?tag=finddupe-20",
+    "lululemon leggings": "https://www.amazon.com/dp/B09XYZ1234?tag=finddupe-20",
+    "gucci belt": "https://www.amazon.com/dp/B07ABC5678?tag=finddupe-20"
+};
+
 function searchDupe() {
-    let query = document.getElementById("dupeSearch").value;
-    if(query) {
-        alert("Searching for: " + query);
-        // Future API integration will go here
-    } else {
-        alert("Please enter a product name.");
+    let query = document.getElementById("dupeSearch").value.toLowerCase();
+    let resultsDiv = document.getElementById("searchResults");
+    resultsDiv.innerHTML = "";
+    
+    for (let key in dupeDatabase) {
+        if (query.includes(key)) {
+            resultsDiv.innerHTML = `<p>Best dupe found: <a href="${dupeDatabase[key]}" target="_blank">Click here</a></p>`;
+            return;
+        }
     }
+    
+    resultsDiv.innerHTML = "<p>No dupes found. Try another search!</p>";
 }
